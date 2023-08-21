d7e4cccefd1cbbeab8e7ba6135477071366fd46b
ViktoriiaZinec <victoria.zinec@gmail.com>
2023-08-21 22:52:33 +0300

    Changes

--- server/index.js
+++ server/index.js
@@ -17,36 +17,3 @@ app.get("/", (req, res) => {
 });
 
 app.listen(8080, () => console.log("Server has started on port 8080"));
-// require("dotenv").config();
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const { Configuration, OpenAIApi } = require("openai");
-
-// const app = express();
-// const { OPENAI_API_KEY } = process.env;
-
-// const configuration = new Configuration({
-//   apiKey: OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// app.listen(8080, () => {
-//   console.log("server started");
-// });
-// app.post("/create", async (req, res) => {
-//   const { prompt } = req.body;
-//   try {
-//     const response = await openai.createImage({
-//       prompt,
-//       n: 1,
-//       size: "512x512",
-//     });
-//     res.send(response.data.data[0].url);
-//   } catch (err) {
-//     res.send(err.message);
-//   }
-// });
